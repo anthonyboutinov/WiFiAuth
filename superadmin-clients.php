774863642a26cad085829330d729ebeac9a63fbb
Anthony Boutinov <anton4488@gmail.com>
2015-06-26 10:57:57 +0300

    +privacy, +superadmin-clients

---
+++ superadmin-clients.php
@@ -0,0 +1,29 @@
+<?php	include 'includes/core/vars.php'; ?>
+<html lang="ru">
+	<head>
+		<?php include 'includes/base/headBootstrapAndBasics.php'; ?>
+	</head>
+
+	<body class="admin-page simple-page">
+		<div class="container glass-panel">
+			<?php include 'includes/base/superadmin-navbar.php'; ?>			
+
+			<div class = "row">
+				<div class = "col-sm-6">
+
+					<h1><i class="fa fa-users"></i> Клиенты</h1> 
+					
+					<div class="page-wrapper">
+
+					 	<table class="table table-hover table-condensed">
+		 					<tr><td class="text-right">2</td><td>27.05<span class="hidden-xs">.2015</span></td><td class="text-center"><i class="fa fa-twitter"></i><span class="sr-only"> Twitter</span></td><td class="text-left">Дубровин Добромысл</td></tr>
+					 	</table>
+
+				 	</div>
+
+				</div>
+         	</div>
+    	</div>
+		<?php include 'includes/base/jqueryAndBootstrapScripts.html'; ?>
+	 	</body>
+</html>
