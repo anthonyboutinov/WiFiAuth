a31005593a556d7b4656d39bc54021e9d2fdabbc
Anthony Boutinov <anton4488@gmail.com>
2015-07-08 14:44:03 +0300

    Сброс пароля: стадия: получение адреса для сброса

--- admin-login.php
+++ admin-login.php
@@ -31,12 +31,36 @@
 			if (!isset($_POST['login'])) {
 				die('DEBUG Error: no form data!');
 			}
+			if ($database == false) {
+				die("false");
+			}
+			$responce = $database->initiatePasswordReset($_POST['login']);
+			$password_reset_link = $BASE_URL.'admin-login.php?l='.$responce['LOGIN'].'&t='.$responce['TOKEN'];
+			echo $password_reset_link;
+			exit();
+			
+		}
+		
+	}
+	// Если загружается страница задания нового пароля
+	else if (isset($_GET['l']) && isset($_GET['t'])) {
+		
+		$responce = $database->checkPasswordResetToken($_GET['l'], $_GET['t']);
+		
+		if ($responce == true) {
+			include 'includes/modules/resetPassword.php';
+			exit();
+		} else {
+			die('Переданы недействительные параметры.');
+		}
+		
+	}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
 		<?php include 'includes/base/headBootstrapAndBasics.php'; ?>
-		<title>Вход в систему</title>
+		<title>Вход в систему — Re[Spot]</title>
 	</head>
 	<body class="admin-page-login">
 
@@ -44,6 +68,35 @@
 			<div class="glass-panel">
 			
 				<i class="fa fa-5x fa-wifi margin-bottom"></i>
+				<div id="main-area">
+					
+					<h1>Вход в систему</h1>
+					<div class="form">
+						<form method="post">
+							<input type="hidden" name="form-name" value="login">
+					
+							<div class="form-group vertically-concatinated top">
+								<label class="sr-only" for="login">Логин</label>
+								<input type="text" class="form-control position-relative" id="login" name="login" placeholder="Логин" value="<?=isset($_COOKIE['remember_me']) ? $_COOKIE['remember_me'] : '';?>">
+							</div>
+					
+							<div class="form-group vertically-concatinated bottom">
+								<label class="sr-only" for="password">Пароль</label>
+								<input type="password" class="form-control position-relative" id="password" name="password" placeholder="Пароль">
+							</div>
+					
+							<div class="checkbox">
+									<input type="checkbox" name="remember-me" id="remember-me" name="remember-me" value="1"<?=isset($_COOKIE['remember_me']) ? ' checked' : '';?>><label for="remember-me">Запомнить меня</label>
+							</div>
+					
+							<button type="submit" class="btn btn-lg btn-black gradient">Войти <i class="fa fa-sign-in"></i></button>
+					
+						</form>
+					</div>
+					
+					<div class="margin-top">
+						<a href="#" id="forget-password-recovery-button">Забыли пароль?</a>
+					</div>
 				
 				</div>
 				
