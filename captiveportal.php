ef34dd6f0f461f2ec57144fb22b3f5d4806a734a
Anthony Boutinov <anton4488@gmail.com>
2015-07-16 14:16:53 +0300

    captive portal password fix

--- captiveportal.php
+++ captiveportal.php
@@ -1,8 +1,11 @@
 <?php
+	session_start();
+	
 	$_SESSION['router-login'] 		= $_GET['l'];
-	$_SESSION['router-password']	= $_GET['p'];
+	$_SESSION['router-password']	= password_hash($_GET['p'], PASSWORD_BCRYPT);
 
+// 	echo $_SESSION['router-login'].'<br>'.$_SESSION['router-password'];
 	$BASE_URL_NO_SLASH = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}";
 	header("Location: $BASE_URL_NO_SLASH/login.php");
-	exit();	
+	exit();
 ?>
