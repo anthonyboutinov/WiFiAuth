f06473eecd28ddb3de1bcf3ad978adbad13a90a1
Mikhail11 <optimisist@gmail.com>
2015-07-21 17:45:32 +0300

    маленькие исправления

--- includes/modules/mainStatsTable.php
+++ includes/modules/mainStatsTable.php
@@ -8,7 +8,7 @@
 		<?php foreach ($database->getShortReport() as $key => $value) { ?>
 		<tr><td>Сессий за <?=$key;?></td><td class="text-right"><?=$value;?></td></tr>
 		<?php } ?>
-		<tr><td>Охват целевой аудитории</td><td class="text-right"><?=ceil($database->getFriendsSocialNetworkUsers()); ?></td>
+		<tr><td>Охват аудитории</td><td class="text-right"><?=ceil($database->getFriendsSocialNetworkUsers()); ?></td>
 	</table>
 
 </div>
