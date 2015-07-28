1f4a461d31b6abd06a5fbbde5f41148f35650533
Mikhail11 <optimisist@gmail.com>
2015-07-28 17:20:53 +0300

    Автопост исправлен и работает

--- autocomplit.php
+++ autocomplit.php
@@ -1,21 +1,13 @@
 <?php
 	require 'includes/core/db_config.php';
 	require 'includes/core/DBWiFiInterface.php';
-echo '<br>4';
  $database = new DBWiFiInterface($servername, $username, $password, $dbname,'','','','','');
-echo '<br>5';
  $tokens = $database->getAccessTokenForVKMessage();
  	if ($tokens->num_rows > 0) {
 		while($row = $tokens->fetch_assoc()) {
-echo '<br>10';
-			echo $row['ID_USER'];
-			echo '<br>'.$row['TOKEN'];
-			echo '<br>'.$row['MESSAGE'];
-			
 			$birthday = $database->getVKBirthdayUsers($row['ID_USER']);
 			if($birthday->num_rows > 0){
 				while($rows = $birthday->fetch_assoc()) {
-
 					$domain = substr($rows['LINK'],15);
 					$url = 'https://api.vk.com/method/messages.send?domain='.$domain
 					.'&message='.urlencode($row['MESSAGE'])
@@ -27,8 +19,6 @@ echo '<br>10';
 					$json = curl_exec($curl);
 					curl_close($curl);
 					}
-
-					echo $json;
 
 				}
 			}
